a6f1832eac106b930017b8b88c917d14a17edfe4
Anika Sian <aetsian@gmail.com>
2019-03-03 15:34:39 -0500

    Completed JsonReaderUtil

---
+++ src/design/AbstractEmployee.java
@@ -0,0 +1,5 @@
+package design;
+
+public abstract class AbstractEmployee implements Employee {
+   AbstractEmployee() {}
+}
--- src/json/parser/JsonReaderUtil.java
+++ src/json/parser/JsonReaderUtil.java
@@ -56,7 +56,11 @@ public class JsonReaderUtil {
                 JsonObject jsonobject = jsonArray.get(i).getAsJsonObject();
                 //you code start here
                 String empEmail = jsonobject.get("empEmail").toString();
+                String empName = jsonobject.get("empName").toString();
+                String salary = jsonobject.get("salary").toString();
+                String department = jsonobject.get("department").toString();
 
+                empList.add(new Employee(empEmail, empName, salary, department));
 
             } catch (Exception ex) {
 
